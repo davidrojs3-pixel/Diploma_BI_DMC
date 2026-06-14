171544433f8eba54e9097eda73b71b1000854ee1
davidrojs3-pixel <davidrojs3@gmail.com>
2026-06-14 17:36:59 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ st.title("Proyecto Final Diploma BI")
 
 st.sidebar.title("Parámetros")
 
-st.image("python-logo.png",width = 50)
-st.sidebar.image("logo-dmc-solo.png",width = 30)
+st.image("python-logo.png",width = 500)
+st.sidebar.image("logo-dmc-solo.png",width = 300)
